3f368ae06130a730f5dffa4868f6e384f8477cd4
roldanmoncada <roldan.h.moncada@gmail.com>
2022-11-23 19:10:07 -0700

    Starting on this with my tutor. Adding comments later.

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const store = require('../db/store');
+
+router.get('/notes', (req, res) => {
+    store.getNotes()
+    .then((notes) => {
+        return res.json(notes)
+    })
+    .catch((err) => {
+        return res.status(500).json(err)
+    })
+})
+
+router.post('/notes', (req, res) => {
+    store.addNote(req.body)
+    .then((note) => {
+        return res.json(note)
+    })
+    .catch((err) => {
+        return res.status(500).json(err)
+    })
+})
+
+module.exports = router;
